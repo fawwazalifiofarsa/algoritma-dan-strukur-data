d25de5f9400c08790cc200a807a96d2328494cf9
fawwazalifiofarsa <fawwaz.alifio.farsa@gmail.com>
2024-02-21 16:18:39 +0700

    Percobaan 1

---
+++ Jobsheet2/Buku10.java
@@ -0,0 +1,29 @@
+package Jobsheet2;
+public class Buku10 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah Halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0 && stok > jml) {
+            stok -= jml;
+        } else {
+            System.out.println("Stock tidak cukup");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
